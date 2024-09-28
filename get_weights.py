c2740220e79e5594c59f9486454e55988d3a0165
Isaac <IsaacJones@hey.com>
2024-09-28 16:31:11 +0100

    Show download progress

--- get_weights.py
+++ get_weights.py
@@ -3,6 +3,8 @@ import requests
 import argparse
 import logging
 
+from tqdm import tqdm
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -15,10 +17,18 @@ def parse_args():
 
 def download_weights(url, save_dir):
     logger.info("Loading weights...")
-    response = requests.get(url)
-    response.raise_for_status()
-    with open(save_dir, "wb") as f:
-        f.write(response.content)
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+        total_size = int(response.headers.get('content-length', 0))
+        block_size = 1024
+        progress = tqdm(total=total_size, unit='iB', unit_scale=True)
+
+        with open(save_dir, 'wb') as f:
+            for data in response.iter_content(block_size):
+                progress.update(len(data))
+                f.write(data)
+        progress.close()
+
     logger.info(f"Model weights saved as {save_dir}")
 
 
