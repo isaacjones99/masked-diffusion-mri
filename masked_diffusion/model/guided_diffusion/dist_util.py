5de98057ba50537b8ba24f1fa547d9dc7b8f027d
Isaac Jones <isaacjones@hey.com>
2024-11-11 20:44:34 +0000

    Add mpi4py.

--- masked_diffusion/model/guided_diffusion/dist_util.py
+++ masked_diffusion/model/guided_diffusion/dist_util.py
@@ -21,6 +21,7 @@ import os
 import socket
 
 import blobfile as bf
+from mpi4py import MPI
 import torch as th
 import torch.distributed as dist
 
@@ -37,20 +38,21 @@ def setup_dist():
     """
     if dist.is_initialized():
         return
+    os.environ["CUDA_VISIBLE_DEVICES"] = f"{MPI.COMM_WORLD.Get_rank() % GPUS_PER_NODE}"
 
-    # Assign a GPU based on the rank of the process.
-    os.environ["CUDA_VISIBLE_DEVICES"] = f"{os.getpid() % GPUS_PER_NODE}"
-
+    comm = MPI.COMM_WORLD
     backend = "gloo" if not th.cuda.is_available() else "nccl"
-   
+
     if backend == "gloo":
         hostname = "localhost"
     else:
         hostname = socket.gethostbyname(socket.getfqdn())
+    os.environ["MASTER_ADDR"] = comm.bcast(hostname, root=0)
+    os.environ["RANK"] = str(comm.rank)
+    os.environ["WORLD_SIZE"] = str(comm.size)
 
-    os.environ["MASTER_ADDR"] = hostname
-    os.environ["MASTER_PORT"] = str(_find_free_port())
-
+    port = comm.bcast(_find_free_port(), root=0)
+    os.environ["MASTER_PORT"] = str(port)
     dist.init_process_group(backend=backend, init_method="env://")
 
 
@@ -59,16 +61,30 @@ def dev():
     Get the device to use for torch.distributed.
     """
     if th.cuda.is_available():
-        return th.device("cuda")
+        return th.device(f"cuda")
     return th.device("cpu")
 
 
 def load_state_dict(path, **kwargs):
     """
-    Load a PyTorch file without redundant fetches across processes.
+    Load a PyTorch file without redundant fetches across MPI ranks.
     """
-    with bf.BlobFile(path, "rb") as f:
-        data = f.read()
+    chunk_size = 2**30  # MPI has a relatively small size limit
+    if MPI.COMM_WORLD.Get_rank() == 0:
+        with bf.BlobFile(path, "rb") as f:
+            data = f.read()
+        num_chunks = len(data) // chunk_size
+        if len(data) % chunk_size:
+            num_chunks += 1
+        MPI.COMM_WORLD.bcast(num_chunks)
+        for i in range(0, len(data), chunk_size):
+            MPI.COMM_WORLD.bcast(data[i : i + chunk_size])
+    else:
+        num_chunks = MPI.COMM_WORLD.bcast(None)
+        data = bytes()
+        for _ in range(num_chunks):
+            data += MPI.COMM_WORLD.bcast(None)
+
     return th.load(io.BytesIO(data), **kwargs)
 
 
@@ -82,10 +98,10 @@ def sync_params(params):
 
 
 def _find_free_port():
-    """
-    Find a free port on the local machine.
-    """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(("", 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
+    finally:
+        s.close()
